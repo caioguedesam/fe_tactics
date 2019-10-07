4bce6f4d078660f6f38753e90739a31f5e40ceed
Caio Guedes <caioguedesam@gmail.com>
2019-10-07 11:15:54 -0300

    Initial commit

---
+++ Assets/Scripts/GridSnap.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSnap : MonoBehaviour
+{
+    // Size of grid to snap
+    public float snapValue = 1;
+
+    // Rounding function
+    private float Round(float input, float roundThreshold)
+    {
+        // Input relative to the snapValue threshold
+        // example: if input = 1 and snapValue = 2, inputRelativeToThreshold = 0.5
+        // as in 1 is 0.5 times 2. Or, if input = 4, inputRelativeToThreshold = 2 and so on...
+        float inputRelativeToThreshold = input / roundThreshold;
+        // Effectively rounding to appropriate value in relation to threshold
+        return roundThreshold * Mathf.Round(inputRelativeToThreshold);
+    }
+
+    private void ChangeCursorPosition(float x, float y)
+    {
+        Vector2 cursorPosition = new Vector2(Round(x, snapValue), Round(y, snapValue));
+        transform.position = new Vector3(cursorPosition.x, cursorPosition.y, transform.position.z);
+    }
+
+    void Update()
+    {
+        Vector3 mouseVector = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        ChangeCursorPosition(mouseVector.x, mouseVector.y);
+    }
+}
---
+++ Assets/Scripts/SpawnWaypoints.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnWaypoints : MonoBehaviour
+{
+    public GameObject waypoint;
+    private List<GameObject> waypointList = new List<GameObject>();
+    public LayerMask waypointLayer;
+
+    private Vector2 currentCursorPosition;
+    private Vector2 oldCursorPosition;
+
+    private void Start()
+    {
+        currentCursorPosition = new Vector2(transform.position.x, transform.position.y);
+    }
+
+    private bool UpdateCursorPosition()
+    {
+        Vector2 newPosition = new Vector2(transform.position.x, transform.position.y);
+        if(newPosition != currentCursorPosition)
+        {
+            oldCursorPosition = currentCursorPosition;
+            currentCursorPosition = newPosition;
+            return true;
+        }
+        return false;
+    }
+
+    private bool CheckExistingWaypoint()
+    {
+        if (Physics2D.OverlapCircle(new Vector2(oldCursorPosition.x, oldCursorPosition.y), 0.5f, waypointLayer))
+            return true;
+        return false;
+    }
+
+    private void Update()
+    {
+        if(UpdateCursorPosition() && !CheckExistingWaypoint())
+        {
+            GameObject newWaypoint = Instantiate(waypoint, oldCursorPosition, Quaternion.identity);
+            waypointList.Add(newWaypoint);
+
+            // Add pathing
+        }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            foreach(GameObject waypointObj in waypointList)
+            {
+                Destroy(waypointObj);
+            }
+        }
+    }
+}
